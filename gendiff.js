b2730fb8db23760d2d3569235841eaf21c12db94
Hohlyandiya <teroryk@gmail.com>
2025-03-25 20:20:48 +0300

    added deep check

--- gendiff.js
+++ gendiff.js
@@ -6,29 +6,77 @@ import _ from 'lodash';
 
 const program = new Command();
 
-const searchDiff = (firstContent, secondContent, distinctiveMark) => {
-    const result = [];
-    for (const [key, value] of Object.entries(firstContent)) {
-        if (Object.hasOwn(secondContent, key) && secondContent[key] === value) {
-            result.push({[`  ${key}`]: value}); 
-        } else {
-            result.push({[`${distinctiveMark} ${key}`]: value});
+const getShiftToTheRight = (obj) => {
+    let result = {};
+    for (const key in obj) {
+        if (obj[key] instanceof Object) {
+            result = {...result, [`  ${key}`]: getShiftToTheRight(obj[key])};
+            continue;
         }
+        result = {...result, [`  ${key}`]: obj[key]}
+    }
+    return result;
+}
+
+const searchDiff = (firstContent, secondContent, distinctiveMark) => {
+    let result = {};
+        for (const key in firstContent) {
+            if (Object.hasOwn(secondContent, key) && firstContent[key] instanceof Object && secondContent[key] instanceof Object) {
+                result = {...result, [`  ${key}`]: searchDiff(firstContent[key], secondContent[key], distinctiveMark)};
+                continue;
+            }
+            if (firstContent[key] instanceof Object ) {
+                firstContent[key] = getShiftToTheRight(firstContent[key]);
+            }
+            result = Object.hasOwn(secondContent, key) && secondContent[key] === firstContent[key] ? 
+            {...result, [`  ${key}`]: firstContent[key]} : 
+            {...result, [`${distinctiveMark} ${key}`]: firstContent[key]};
+        }
+    return result;
+}
+
+const sortContent = (arrData) => {
+    const ignoringIndentation = 2;
+    const result = Object.keys(arrData)
+    .sort((a, b) => {
+        const nameA = a.slice(ignoringIndentation);
+        const nameB = b.slice(ignoringIndentation );
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    })
+    .reduce((acc, key) => {
+        acc[key] = arrData[key] instanceof Object ? sortContent(arrData[key]) : arrData[key];
+        return acc;
+    }, {});
+    return result;
+}
+
+const stringify = (obj, repeatChar, repeatAmount, deep = 1) => {
+    let result = '';
+    const leftShift = 2;
+    const offsetIndent = deep * repeatAmount - leftShift;
+    const indentWithoutOffset = deep * repeatAmount;
+    for (const key in obj) {
+        if (obj[key] instanceof Object) {
+            result = `${result}\n${repeatChar.repeat(offsetIndent)}${key}: {${stringify(obj[key], repeatChar, repeatAmount, deep + 1)}\n${repeatChar.repeat(indentWithoutOffset)}}`;
+            continue;
+        }
+        result = `${result}\n${repeatChar.repeat(deep * repeatAmount - leftShift)}${key}: ${obj[key]}`;
     }
     return result;
 }
 
 export const genDiff = (firstFileContent, secondFileContent) => {
+
     const firstDiffContent = searchDiff(firstFileContent, secondFileContent, '-');
     const secondDiffContent = searchDiff(secondFileContent, firstFileContent, '+');
-    const allContent = [...firstDiffContent, ...secondDiffContent];
-    const indent = 2;
-    
-    const result = _.sortBy(allContent, (object) => Object.keys(object)
-    .map(element => element.slice(indent)))
-    .reduce((acc, elem) => Object.assign(acc, elem), {});
-
-    return JSON.stringify(result, null, indent).replace(/"|,/g, '');
+    const allContent = sortContent(_.merge(firstDiffContent, secondDiffContent));
+    return `{${stringify(allContent, ' ', 4)}\n}`;
 };
 
 program
@@ -40,11 +88,11 @@ program
 .helpOption('-h, --help', 'output usage information')
 .action((filepath1, filepath2) => {
     const listFilepath = [filepath1, filepath2];
-    //const listFilesContent = listFilepath.map((filepath) => JSONparse(filepath));
+    const index = -1;
     const listFilesContent = listFilepath.map((filepath) => {
-        if (filepath.split('.').at(-1) === 'yaml' || filepath.split('.').at(-1) === 'yml') {
+        if (filepath.split('.').at(index) === 'yaml' || filepath.split('.').at(index) === 'yml') {
             return YAMLparse(filepath);
-        } if (filepath.split('.').at(-1) === 'json') {
+        } if (filepath.split('.').at(index) === 'json') {
             return JSONparse(filepath);
         }
         throw new Error('Unsupported file extension');
