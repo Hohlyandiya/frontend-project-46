d4933f81b0fd4814693fa7b6205b20dc83140caf
Hohlyandiya <teroryk@gmail.com>
2025-04-23 00:10:35 +0300

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,18 @@
 import { defineConfig } from "eslint/config";
-/* import jest from "eslint-plugin-jest";
-import globals from "globals"; */
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-/* import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc"; */
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-/* const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-}); */
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([{
-/*     extends: compat.extends("airbnb-base", "plugin:jest/recommended"),
 
-    plugins: {
-        jest,
-    },
+  plugins: {
+    '@stylistic': stylistic
+  },
 
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-        },
+  linterOptions: {
+    reportUnusedInlineConfigs: "error",
+  },
 
-        ecmaVersion: "latest",
-        sourceType: "module",
-    }, */
-
-    rules: {
-        "no-console": 0,
-
-/*         "import/extensions": ["error", "ignorePackages", {
-            js: "always",
-        }], */
-
-        "no-underscore-dangle": [2, {
-            allow: ["__filename", "__dirname"],
-        }],
-    },
+  rules: {
+    "no-console": 0,
+    '@stylistic/indent': ['error', 2],
+  },
 }]);
